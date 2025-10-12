4790e831983c37905eca8f7676d53237b6aea2eb
Léo Mindlin <hello@leomindlin.com>
2025-10-12 15:21:27 -0700

    fix: add font

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://interstellar.leomindlin.com",
+  site: "https://leosm-07.github.io/mcgill-iferg/",
   trailingSlash: "ignore",
   output: "static",
   vite: {
